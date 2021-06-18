f7da3e2bc4d14c9c938bd691d099b18b4b4cfc00
NickMateus11 <mateus.nicholas@gmail.com>
2021-06-17 23:07:45 -0400

    phase screen gaussian prop

--- phase_screen_test.py
+++ phase_screen_test.py
@@ -1,7 +1,9 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+from gaussian_beam import GaussianBeam
 from func_utils import str_fnc2_ft, circ, plot_slice, mesh
+from propagation import fresnel_prop_no_scale
 from phase_screen import ft_sub_harm_phase_screen, ft_phase_screen
 
 
@@ -71,9 +73,49 @@ def phase_screen_statistics():
     plt.show()    
 
 
+def phase_screen_prop():
+    D = 2
+    r0 = 0.1
+    N = 256
+    L0 = 100
+    l0 = 0.01
+
+
+    delta = D/N
+    x = np.linspace(-N/2,N/2-1, N) * delta
+    y = np.linspace(-N/2,N/2-1, N) * delta
+
+    [phz_lo, phz_hi] = ft_sub_harm_phase_screen(r0, N, delta, L0, l0)
+    phz = phz_lo + phz_hi # (SH + FT)
+    
+    # mask  = circ(x, y, D*0.9)
+    # phz *= mask
+
+    # N = 512
+    wvl = 600e-9
+    w0 = 1e-3
+    dz = 5
+    L = 5*w0
+    delta = L/N
+
+    [x1,y1] = np.meshgrid(
+        delta * np.linspace(-N//2,N//2-1,N),
+        delta * np.linspace(-N//2,N//2-1,N),
+    )
+
+    G = GaussianBeam(wvl, w0, 0, delta, N)
+    g_prop = G.wave * phz
+    # g_prop2,x2,y2 = fresnel_prop_no_scale(g_prop, wvl, delta, 5)
+    # mesh(x2,y2,np.abs(g_prop2)**2)
+    mesh(x1,y1,np.abs(g_prop)**2)
+
+    plt.show()
+
+
 def main():
     # phase_screen_gen()
     phase_screen_statistics()
+    # phase_screen_prop()
 
 
 if __name__ == '__main__':
